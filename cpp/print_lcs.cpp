1f83f81e3f8c0a540a2338cce1aeaf53a76028ec
Malay Agarwal <malay.agarwal261016@outlook.com>
2022-08-20 23:58:58 +0530

    Solve Print LCS

---
+++ cpp/print_lcs.cpp
@@ -0,0 +1,62 @@
+#include <bits/stdc++.h>
+
+#define ll long long int
+#define newl "\n"
+
+using namespace std;
+
+int topDown(int i, int j, string &s, string &t, vector<vector<int>> &dp)
+{
+    if (i < 0 || j < 0)
+        return 0;
+
+    if (dp[i][j] != -1)
+        return dp[i][j];
+
+    if (s[i] == t[j])
+        return dp[i][j] = 1 + topDown(i - 1, j - 1, s, t, dp);
+
+    int s_left = topDown(i - 1, j, s, t, dp);
+    int t_left = topDown(i, j - 1, s, t, dp);
+
+    return dp[i][j] = max(s_left, t_left);
+}
+
+vector<vector<int>> lcs(string &s, string &t)
+{
+    int n = s.length();
+    int m = t.length();
+
+    vector<vector<int>> dp(n, vector<int>(m, -1));
+
+    int res = topDown(n - 1, m - 1, s, t, dp);
+
+    return dp;
+}
+
+string find_lcs(int i, int j, vector<vector<int>> &grid, string &s, string &t)
+{
+    if (i < 0 || j < 0)
+        return "";
+
+    if (s[i] == t[j])
+        return find_lcs(i - 1, j - 1, grid, s, t) + s[i];
+
+    if (grid[i - 1][j] > grid[i][j - 1])
+        return find_lcs(i - 1, j, grid, s, t);
+
+    return find_lcs(i, j - 1, grid, s, t);
+}
+
+int main(void)
+{
+    string s = "abcde";
+    string t = "bdgek";
+
+    vector<vector<int>> grid = lcs(s, t);
+
+    string res = find_lcs(s.length() - 1, t.length() - 1, grid, s, t);
+
+    cout << res << newl;
+    return 0;
+}
