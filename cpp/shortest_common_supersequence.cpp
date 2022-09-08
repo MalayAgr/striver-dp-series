1181b93a7d97cce8fbb6bd662aff5c3684327b83
Malay Agarwal <malay.agarwal261016@outlook.com>
2022-09-08 17:40:02 +0530

    Remove commented code

--- cpp/shortest_common_supersequence.cpp
+++ cpp/shortest_common_supersequence.cpp
@@ -5,23 +5,6 @@
 
 using namespace std;
 
-// int topDown(int i, int j, string &s, string &t, vector<vector<int>> &dp)
-// {
-//     if (i < 0 || j < 0)
-//         return 0;
-
-//     if (dp[i][j] != -1)
-//         return dp[i][j];
-
-//     if (s[i - 1] == t[j - 1])
-//         return dp[i][j] = 1 + topDown(i - 1, j - 1, s, t, dp);
-
-//     int s_left = topDown(i - 1, j, s, t, dp);
-//     int t_left = topDown(i, j - 1, s, t, dp);
-
-//     return dp[i][j] = max(s_left, t_left);
-// }
-
 vector<vector<int>> lcs(string &s, string &t)
 {
     int n = s.length();
